0ae21c5b8483bd927f72708a0edc07777a3607e9
mishra744542 <49001345+mishra744542@users.noreply.github.com>
2020-08-13 14:56:45 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,5 @@
+var name=prompt('enter your name');
+var age=prompt('enter your age');
+agedays=age*365.25
+remainingdays=(60-age)*365.25;
+alert('hellow ' + name + '\nyour current age:' + agedays +'days\nyou will be there on the earth only ' +remainingdays +'days.no one can change including god also')
